dc2cebe89d2a056043529050832959ed371f807a
tsondru <tsondru@sustia.com>
2025-09-30 15:15:32 -0600

    Remove unused Event wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 pub mod subsystems {
-    pub mod sdb;
     pub mod agent;
     pub mod agents;
+    pub mod sdb;
 }
-pub mod sdb_server;
-pub mod message;
-pub mod settings;
 pub mod logger;
-pub mod event;
+pub mod message;
+pub mod sdb_server;
+pub mod settings;
